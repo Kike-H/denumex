2436cb22669e00de96787fb089be18f9a23e4561
Manuel E. Hernandez <kicke10h@gmail.com>
2022-03-05 03:16:15 -0600

    Welcome and singUp

---
+++ lib/components/InputFile.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class InputFile extends StatelessWidget {
+  final String label;
+  final bool obscureText;
+  TextEditingController controller = TextEditingController();
+
+  InputFile({required this.label, this.obscureText = false, Key? key})
+      : super(key: key);
+
+  String getValue() {
+    return controller.text.toString();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          const SizedBox(
+            height: 5,
+          ),
+          TextField(
+            obscureText: obscureText,
+            decoration: InputDecoration(
+                labelText: label,
+                contentPadding:
+                    const EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                enabledBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey)),
+                border: const OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey))),
+            controller: controller,
+          ),
+          const SizedBox(height: 10)
+        ]);
+  }
+}
---
+++ lib/screens/singup.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:denumex/components/InputFile.dart';
+import 'package:flutter/services.dart';
+
+class SingUp extends StatelessWidget {
+  final inputName = InputFile(label: "Nombre(s)");
+  final inputLastName = InputFile(label: "Apellido");
+  final inputEmail = InputFile(label: "Email");
+  final inputPassword = InputFile(label: "Contraseña");
+  final inputPasswordConfirm = InputFile(label: "Confirma tu contraseña");
+
+  SingUp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            size: 20,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          height: MediaQuery.of(context).size.height - 50,
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  const Text(
+                    "Sign up",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    "Create an account, It's free ",
+                    style: TextStyle(fontSize: 15, color: Colors.grey[700]),
+                  )
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  inputName,
+                  inputEmail,
+                  inputPassword,
+                  inputPasswordConfirm,
+                ],
+              ),
+              Container(
+                padding: const EdgeInsets.only(top: 3, left: 3),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    border: const Border(
+                      bottom: BorderSide(color: Colors.black),
+                      top: BorderSide(color: Colors.black),
+                      left: BorderSide(color: Colors.black),
+                      right: BorderSide(color: Colors.black),
+                    )),
+                child: MaterialButton(
+                  minWidth: double.infinity,
+                  height: 60,
+                  onPressed: () {},
+                  color: const Color(0xff0095FF),
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: const Text(
+                    "Sign up",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const <Widget>[
+                  Text("Already have an account?"),
+                  Text(
+                    " Login",
+                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
